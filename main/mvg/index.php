52ec93841f7fd9cdacd6437ad5dfee7a4e211b8b
siMobin <shakibulislammobin@gmail.com>
2023-12-08 23:20:46 +0600

    upload Mission, Vision and Goals
    	new file:   main/mvg/index.php
    	modified:   main/nav.php
    	new file:   main/style/mvg.scss

---
+++ main/mvg/index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/mvg.css">
+    <title>Mission, Vision and Goals</title>
+</head>
+
+<body>
+    <?php
+    require '../nav.php';
+    // include './header_slider.php';
+    ?>
+
+    <section id="about">
+
+        <div class="container">
+
+
+            <div class="services_item title">
+                <strong>Our Mission</strong>
+            </div>
+
+            <div id="mission_div">
+                <p>The mission of Stamford University, Bangladesh is to provide and facilitate global standard of education to its students of all walks of life irrespective of gender, religion, cast, creed, color, age, time and place, with a view to preparing them for leadership and service in multicultural, global and technological societies by helping the students in succeeding academically and in the transition from university life to productive life.</p>
+                <h5><strong>Stamford University is based on the following Principles:</strong></h5>
+
+                <div class="box">
+
+                    <div>
+                        <p><span><i class="fa-solid fa-circle-check"></i></span>Education should be open to all and irrespective of gender, religion, cast, color, age, time and place.</p>
+                        <p><span><i class="fa-solid fa-circle-check"></i></span>Education should be of international standard and must have universal recognition.</p>
+                        <p><span><i class="fa-solid fa-circle-check"></i></span>Opportunity to learn should be open to anyone with the desire to achieve.</p>
+                        <p><span><i class="fa-solid fa-circle-check"></i></span>Education should be of international standard and must have universal recognition.</p>
+                        <p><span><i class="fa-solid fa-circle-check"></i></span>Education is a life long process and must be relevant to the student's aspirations.</p>
+                        <p><span><i class="fa-solid fa-circle-check"></i></span>Study and student's should be the first priority. The primary function of faculty/ staff/ administration should be to help the students in succeeding academically and in the transition from university life to productive life as members of society.</p>
+                        <p><span><i class="fa-solid fa-circle-check"></i></span>To provide a quality education to students, free from parochialism and make them cosmopolitan and global in the true sense.</p>
+                    </div>
+
+                    <img src="https://images.pexels.com/photos/1462630/pexels-photo-1462630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+
+                </div>
+            </div>
+
+            <br>
+
+            <div class="services_item title">
+                <strong>Our Vision</strong>
+            </div>
+
+            <div id="vision_div">
+
+                <p>Stamford University, Bangladesh is moving forward with a vision of Excellence - <span style="font-weight: bold;color: #03407c;">"Moving Educational Excellence from Good to Great"</span>. Form good to better to excellent and from excellent to great. With a long range Strategic Planning the University continues the transformative process of being, one of the premier universities of higher education in this region and it is evident by the milestones that have been set by Stamford Group in Bangladesh over te last 10 years. The current plan articulates new directions for the university.</p>
+
+                <div class="box">
+
+                    <img src="https://images.pexels.com/photos/1462630/pexels-photo-1462630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+
+                    <div>
+
+                        <h5><strong>This strategic plan outlines an enhanced vision of excellence in the following areas of the university:</strong></h5>
+
+                        <p><span><i class="fad fa-badge-check"></i></span>Updated and global standard academic programs.</p>
+                        <p><span><i class="fad fa-badge-check"></i></span>Rich faculty members to prepare students for future leadership and services in multicultural, global and technological societies.</p>
+                        <p><span><i class="fad fa-badge-check"></i></span>Continuation of creating possible better facilities for students.</p>
+                        <p><span><i class="fad fa-badge-check"></i></span>Greater emphaisis on student success beyond the wall of Stamford.</p>
+                        <p><span><i class="fad fa-badge-check"></i></span>Expected scholarly contribution in the area of instruction, research, creative expression and service.</p>
+                        <p><span><i class="fad fa-badge-check"></i></span>Continuous development and expansion in all areas and effort to develop pertnership at all important level.</p>
+                        <p><span><i class="fad fa-badge-check"></i></span>Continuous reviw of the factors involved in new dierctions.</p>
+                        <p><span><i class="fad fa-badge-check"></i></span>Identifying problems in different areas and keep on repairing them.</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <br>
+
+            <div class="services_item title">
+                <strong>Goals and Objectives</strong>
+            </div>
+
+            <div id="goals_div">
+
+                <p>The Board of Directors of Stamford University has affirmed a statement of goals and objectives to guide the university's development.</p>
+
+                <div class="box">
+
+                    <div>
+
+                        <h5><strong>The specific goals and objectives of the university are as follows:</strong></h5>
+                        <p><span><i class="fa fa-bullseye"></i></span>To serve the intellectual needs and professional aspirations of its students and to contribute to the intellectual and professional development of the society.</p>
+                        <p><span><i class="fa fa-bullseye"></i></span>To create for students the environment and programs that help to satisfy current needs and anticipate future ones.</p>
+                        <p><span><i class="fa fa-bullseye"></i></span>To establish different educational programs in global standard in line with the universities of the USA &amp; the UK with their support and recognition which will facilitate our students to get access into international job market in these fields directly; as a result, our country will be able to earn valuable foreign currency.</p>
+                        <p><span><i class="fa fa-bullseye"></i></span>To provide Global Standard Education (GSE) in Bangladesh.</p>
+                        <p><span><i class="fa fa-bullseye"></i></span>Our Research and Monitoring Team &ldquo;RMT&rdquo; will be in search of new trends in education in developed countries and identify that and evaluate their adaptability in Bangladesh to keep constantly updating the standard of education to match with the &ldquo;GSE&rdquo;;</p>
+
+                    </div>
+                    <img src="https://images.unsplash.com/photo-1579019163248-e7761241d85a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                </div>
+
+                <div class="box">
+                    <img src="https://images.pexels.com/photos/5940841/pexels-photo-5940841.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                    <div>
+                        <p><span><i class="fa fa-bullseye"></i></span>To develop the degrees of the university up to GSE level through the membership/accreditation of internationally recognized accreditation bodies like ACBSP, AACSB and to establish our degrees equivalent to the degrees of any good university in the USA through World Education Services (WES) the degree equivalence authority to recognize degrees of countries outside the USA.</p>
+                        <p><span><i class="fa fa-bullseye"></i></span>To establish joint degree programs &amp; credit transfer programs with some U.S. universities such as Claflin University, New York University (NYU) &amp; Stamford University, USA to facilitate quality education and make foreign degrees available to students in their native country.</p>
+                        <p><span><i class="fa fa-bullseye"></i></span>To provide high quality education for development of human resource having the competence to compete in the contemporary world market.</p>
+                        <p><span><i class="fa fa-bullseye"></i></span>To conduct and support research for the advancement of knowledge as well as the socio-economic development of the country through the application of technology-oriented knowledge.</p>
+                        <p><span><i class="fa fa-bullseye"></i></span>To Contribute to moral and ethical development of the common people for the creation of a better society, free from corruption, and co-existing in peace, harmony and good environment in the society.</p>
+                        <p><span><i class="fa fa-bullseye"></i></span>To create a congenial academic environment for the youth for their intellectual advancement, free from political and other disturbances.</p>
+                        <p><span><i class="fa fa-bullseye"></i></span>To ensure dissemination of knowledge under the coverage of ethical, human and rational approach reflecting national heritage, values and principles of Bangladesh with a view to producing graduates who will be sound professionally and upright morally.</p>
+                        <p><span><i class="fa fa-bullseye"></i></span>To create an opportunity to serve education for all groups of people in the country, including common people of rural and urban areas, fresh students as well as jobholders through on campus, online and distance learning education.</p>
+                        <p><span><i class="fa fa-bullseye"></i></span>To develop and train students to meet the gradually increasing demand for efficient graduates and professionals in Bangladesh as well as in the international job market, including the USA &amp; Canada through special study programs in specific fields.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <br>
+    <?php
+    require '../footer.php';
+
+    ?>
+
+</body>
+
+</html>
