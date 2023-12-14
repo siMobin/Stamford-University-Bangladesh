c36b31226f6eea223f2f25ca1eb8f999d0e6c238
siMobin <shakibulislammobin@gmail.com>
2023-12-14 15:47:05 +0600

    upload database for (100644)
    	modified:   Database/courses.sql

--- Database/courses.sql
+++ Database/courses.sql
@@ -10,5 +10,19 @@ batch varchar (10) not null
 create table CRS_confirm(
 course_code varchar(20) not null,
 studentID varchar(50) not null,
-semester varchar(50) not null
+semester varchar(50) not null,
+batch varchar (10),
+mid varchar(50),
+final varchar(50),
+30% varchar(50)
+)
+
+
+create table course_load(
+course_code varchar(20) not null,
+course_name varchar(255),
+facultyID varchar(50) not null,
+semester varchar(50) not null,
+department varchar(10) not null,
+batch varchar(10) not null
 )
