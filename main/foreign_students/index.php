f19dae69f584f804873eaffc59d881d0978bf8ea
siMobin <shakibulislammobin@gmail.com>
2023-12-15 18:47:43 +0600

    Upload =>
    	new file:   main/downloads/index.php
    	new file:   main/foreign_students/index.php
    	new file:   main/grading_system/index.php
    	modified:   main/style/_root.scss
    	new file:   main/style/downloads.scss
    	new file:   main/style/foreign_students.scss
    	new file:   main/style/grading_system.scss

---
+++ main/foreign_students/index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="../images/logo.png">
+    <link rel="stylesheet" href="../style/foreign_students.css">
+    <title>SUB-International Students</title>
+</head>
+
+<?php
+$table = ' 
+
+<table border="1" style=" border-collapse: collapse; cellpadding: 10px;">
+<thead>
+    <tr>
+        <th>SL#</th>
+        <th>Programs</th>
+        <th>Fees (BD Taka)</th>
+        <th>Service Office</th>
+        <th>Remarks</th>
+    </tr>
+</thead>
+<tbody>
+    <tr>
+        <td>01</td>
+        <td>Admission Fee: Bachelor / Honor’s Programs</td>
+        <td>
+            Admission Fee: 15,000<br>
+            Development Fee: 8,000<br>
+            Medical Fee: 3,000<br>
+            ID Card Fee: 200<br>
+            Library Card Fee: 200<br>
+            Service Charge: 20,000<br>
+            <strong>Total BD. TK.46,400/-</strong>
+        </td>
+        <td rowspan="3">Admission Office, Ground floor, Siddeswari Campus, 51 Siddeswari Road (Ramna), Dhaka-1217.</td>
+        <td>
+            Admission Fees: One time, non-refundable.<br>
+            Other Fees: One time.<br>
+            All fees will be paid at the time of Admission Total BD. TK.46,400/.<br>
+            <strong>Requirement of Admission documents:</strong><br>
+            Original & Photocopy of all academic Certificates & Mark sheets,<br>
+            Original & Photocopy of Passport with BD visa (main pages),<br>
+            Offer letter Original & Photocopy & 04 Passport size photographs & 02 Stamp size photographs.
+        </td>
+    </tr>
+    <tr>
+        <td>02</td>
+        <td>Admission Fee: Master Programs (Business Administration, Arts, Social Sciences & Law)</td>
+        <td>
+            Admission Fee: 15,000<br>
+            Library Fee: 2,500<br>
+            Development Fee: 8,000<br>
+            Medical Fee: 1,500<br>
+            ID Card Fee: 200<br>
+            Library Card Fee: 200<br>
+            Service Charge: 20,000<br>
+            <strong>Total BD. TK.47,400/-</strong>
+        </td>
+        <td>
+            Admission Fees: One time, non-refundable.<br>
+            Other Fees: One time.<br>
+            All fees will be paid at the time of Admission Total BD. TK.47,400/.<br>
+            <strong>Requirement of Admission documents:</strong><br>
+            Original & Photocopy of all academic Certificates & Mark sheets,<br>
+            Original & Photocopy of Passport with BD visa (main pages),<br>
+            Offer letter Original & Photocopy & 04 Passport size photographs & 02 Stamp size photographs.
+        </td>
+    </tr>
+    <tr>
+        <td>03</td>
+        <td>Admission Fee: Master Programs (Science & Engineering)</td>
+        <td>
+            Admission Fee: 15,000<br>
+            Library Fee: 2,500<br>
+            Development Fee: 8,000<br>
+            Lab Fee: 5,000<br>
+            Medical Fee: 1,500<br>
+            ID Card Fee: 200<br>
+            Library Card Fee: 200<br>
+            Service Charge: 20,000<br>
+            <strong>Total BD. TK.52,400/-</strong>
+        </td>
+        <td>
+            Admission Fees: One time, non-refundable.<br>
+            Other Fees: One time.<br>
+            All fees will be paid at the time of Admission Total BD. TK.52,400/.<br>
+            <strong>Requirement of Admission documents:</strong><br>
+            Original & Photocopy of all academic Certificates & Mark sheets,<br>
+            Original & Photocopy of Passport with BD visa (main pages),<br>
+            Offer letter Original & Photocopy & 04 Passport size photographs & 02 Stamp size photographs.
+        </td>
+    </tr>
+</tbody>
+</table>
+';
+
+require_once('../../vendor/autoload.php');
+
+/**
+ * Generates a PDF document with the given table content.
+ *
+ * @param mixed $table The content of the table to be included in the PDF.
+ * @throws TCPDF_Exception If there is an error during the PDF generation.
+ * @return string The generated PDF document as a string.
+ */
+function generatePDF($table)
+{
+    $auth = "<small style='font-size: 10px;'>Generated on " . date('d-m-Y H:i:s') . "<br>Stamford University Bangladesh</small>";
+    // Create instance of TCPDF
+    $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+    // Set document metadata
+    $pdf->SetCreator('SUB');
+    $pdf->SetAuthor('siMobin');
+    $pdf->SetTitle('SUB-Requirement of Admission Fees & documents');
+    $pdf->SetSubject('Requirement of Admission Fees & documents');
+    $pdf->SetKeywords('SUB');
+
+    // Add a page
+    $pdf->AddPage();
+
+    // Set font
+    $pdf->SetFont('times', '', 12);
+
+    // Your content goes here - use TCPDF methods to add content
+    $pdf->writeHTML("$table$auth", true, false, true, false, '');
+
+    // Output PDF as a string
+    $output = $pdf->Output('output.pdf', 'S');
+
+    return $output;
+}
+
+if (isset($_POST['download_pdf'])) {
+    $table = $table;
+
+    // Call the function to generate the PDF
+    $pdfContent = generatePDF($table);
+
+    // Output the PDF to the browser
+    header('Content-Type: application/pdf');
+    header('Content-Disposition: attachment; filename="Requirement of Admission Fees & documents.pdf"');
+    echo $pdfContent;
+    exit;
+}
+
+?>
+
+
+<body>
+
+    <header>
+        <?php
+        require '../nav.php';
+        // include './header_slider.php';
+        ?>
+    </header>
+
+    <div class="fssc">
+        <h1>Functioning of FSSC</h1>
+        <h3>Duties and responsibilities of the Cell:</h3>
+        <p>Issue Admission Offer letter to foreign candidates seeking admission complying requisite criteria of a relevant program.</p>
+
+        <p>Receiving the students from the Airport on completion of immigration procedure.</p>
+
+        <p>Arrange transportation for such students from Airport to the University Admission office.</p>
+
+        <p>Arrange lunch/ dinner for them after receiving them from the Airport.</p>
+
+        <p>Ensure accommodation in the dormitory arranged for them.</p>
+
+        <p>Complete their admission procedure from the Admission Office and arrange ID Card for them.</p>
+
+        <p>Inform the arrival of such students with their bio-data and photocopy of passports to Depts. concerned.</p>
+
+        <p>Ensure their requisite medical tests and advice by the University Medical Center.</p>
+
+        <p>Ensure documentation of the students by the Admission Office.</p>
+
+        <p>Process the Visa extension of students with the concerned Govt. Offices.</p>
+
+        <p>Implementation of decisions taken by the Foreign Students Service Committee as and when given.</p>
+
+        <p>Solve difficulties and welfare matters relating to foreign students.</p>
+
+        <p>Explain necessary rules and regulations for the students of the University.</p>
+
+        <p>Ensure safe custody of Passports of foreign students.</p>
+
+        <p>Updating Website relating to foreign students information on admission and other instructions.</p>
+
+        <p>Ensure management of Foreign students dormitory ( regarding rent agreement/utility payment etc. through the students committee).</p>
+
+    </div>
+
+    <h1 class="initial-fac h1">Initial Facilities</h1>
+    <p class="initial-fac">Receiving the Students from the Airport. University will arrange transportation from the Airport (Charge Free).Arrange first time Lunch or Dinner (Charge Free), University will arrange accommodation in the hired dormitory. Students will complete admission procedure with the help of FSSC from the Admission Office and will receive ID card.</p>
+
+    <div class="enroll">
+        <h1>Enrollment Requirement</h1>
+        <p> Offer Letter for Enrolment: No Fees required (Will be issued from the university after proper scrutiny of educational requirements.)</p>
+
+        <p> Send soft copy Prescribed CV or Bio-Data (MS Word) & Scanned copy documents by Email at: <a href="mailto:fssc@stamforduniversity.edu.bd">fssc@stamforduniversity.edu.bd</a></p>
+
+        <p> Educational requirements & documents :</p>
+        <table border='1' style=' border-collapse: collapse;'>
+            <thead>
+                <tr>
+                    <th>SL#</th>
+                    <th>Programs</th>
+                    <th>Required documents</th>
+                    <th>Prescribed CV or Bio-Data (MS Word)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>01</td>
+                    <td>For Bachelor / Honor’s Programs</td>
+                    <td>Educational Requirement: See Admission Requirement.<br>Prescribed CV or Bio-data, Original Valid Passport main 1 or 2 pages<br>Scanned copy or Photocopy (valid Minimum 12 months).<br>Scanned copy or Photocopy of SSC & HSC or O-Level & A-level original Certificates & Mark Sheets.</td>
+                    <td><a class="submit" href="../downloads/">Download</a></td>
+                </tr>
+                <tr>
+                    <td>02</td>
+                    <td>For Master Programs</td>
+                    <td>Educational Requirement: See Admission Requirement.<br>Prescribed CV or Bio-data, Original Valid Passport main 1 or 2 pages<br>Scanned copy or Photocopy (valid Minimum 12 months).<br>Scanned copy or Photocopy of SSC, HSC or O-Level, A-level & Honor’s or Bachelor original Certificates & Mark Sheets.</td>
+                    <td><a class="submit" href="../downloads/">Download</a></td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+
+
+    <div class="requirement">
+        <details>
+            <summary>
+                Requirement of Admission Fees & documents
+            </summary>
+
+            <?php
+            echo $table;
+            ?>
+            <br>
+            <!-- <br> -->
+            <form action="" method="post">
+                <button class="submit" type="submit" name="download_pdf">Download PDF</button>
+            </form>
+
+        </details>
+    </div>
+
+    <details>
+        <summary>Necessary Information, documents and Fees for Foreign Students VISA extension or renewal: Please see the website of visa office Government of Bangladesh and see the requirements below</summary>
+
+        <div class="details">
+            <h1>Dept. of Immigration & Passports</h1>
+
+            <p><strong>Visa office address :</strong>Agargaon, Dhaka, Bangladesh</p>
+
+            <p><strong>Visit Visa office website :</strong><a href="www.visa.gov.bd">www.visa.gov.bd</a></p>
+
+            <p><strong>Hotline :</strong>333</p>
+
+            <p><strong>Working time of Visa office: 9:00 AM to 3:00 PM (Friday, Saturday & Govt. Holiday closed)</strong></p>
+        </div>
+    </details>
+
+    <?php
+    require '../footer.php';
+    ?>
+</body>
+
+</html>
---
+++ main/grading_system/index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/grading_system.css">
+    <link rel="icon" type="image/x-icon" href="../images/logo.png">
+    <title>Grading System</title>
+</head>
+
+<body>
+    <header>
+        <?php
+        require '../nav.php';
+        ?>
+    </header>
+
+    <h1>Evaluation Procedures</h1>
+    <p>The performance of a student in any course will be evaluated on the basis of class attendance, in course/class test/class assessment, assignments/term papers/case, mid-term and semester final examinations.</p>
+    <strong>For All Programs:</strong>
+    <p>A theory course will be divided into two parts for teaching and the course evaluation as follows:</p>
+
+    <div class="table_wrapper">
+        <table border="1">
+            <tr>
+                <th>1st Part</th>
+                <th></th>
+                <th>2nd Part</th>
+                <th></th>
+            </tr>
+            <tr>
+                <td>Case/Assignment</td>
+                <td>5%</td>
+                <td>Case/Assignment</td>
+                <td>5%</td>
+            </tr>
+            <tr>
+                <td>Incourse/Class test</td>
+                <td>10%</td>
+                <td>Incourse/Class test</td>
+                <td>10%</td>
+            </tr>
+            <tr>
+                <td>Midterm</td>
+                <td>30%</td>
+                <td>Semester Final</td>
+                <td>30%</td>
+            </tr>
+            <tr>
+                <td colspan="4">Attendance: 10%</td>
+            </tr>
+        </table>
+    </div>
+
+    <!--  -->
+    <!--  -->
+
+    <p>* For a 3 credit hours course minimum four class tests need to be taken and best two of them will be considered for evaluation. For a 2 credit hours course minimum three class tests need to be taken and best two of them will be considered for evaluation.</p>
+    <p>** After the completion of the midterm examination, the 1st part of the course would be considered completed and would not be carried for semester final examination.</p>
+
+
+    <h1>Grading System</h1>
+    <p>The performance of a student in a given course is made through continuous evaluation that comprises quizzes/ in-course, class participation, attendance, home work/assignment, case study, mid-term and semester final examinations.</p>
+
+    <p>Letter grades and grade points are used to evaluate the performance of a student in a given course. A+, A, A-, B+, B, B-, C+, C and D are the passing grades while F is the failing grade. Grade ‘I’ is incomplete and grade ‘W’ is for withdraw. The numerical grades used in calculating the GPA/CGPA (Cumulative grade point average) are as follows:</p>
+
+    <div class="table_wrapper">
+        <table border="1">
+            <tr>
+                <th>Marks Range</th>
+                <th>Letter Grade</th>
+                <th>Grade Point</th>
+            </tr>
+            <tr>
+                <td>80% and above</td>
+                <td>A+</td>
+                <td>4.0</td>
+            </tr>
+            <tr>
+                <td>75% to less than 80%</td>
+                <td>A</td>
+                <td>3.75</td>
+            </tr>
+            <tr>
+                <td>70% to less than 75%</td>
+                <td>B+</td>
+                <td>3.5</td>
+            </tr>
+            <tr>
+                <td>65% to less than 70%</td>
+                <td>B</td>
+                <td>3.25</td>
+            </tr>
+            <tr>
+                <td>60% to less than 65%</td>
+                <td>B-</td>
+                <td>3.0</td>
+            </tr>
+            <tr>
+                <td>55% to less than 60%</td>
+                <td>C+</td>
+                <td>2.75</td>
+            </tr>
+            <tr>
+                <td>50% to less than 55%</td>
+                <td>C</td>
+                <td>2.50</td>
+            </tr>
+            <tr>
+                <td>45% to less than 50%</td>
+                <td>D</td>
+                <td>2.25</td>
+            </tr>
+            <tr>
+                <td>40% to less than 45%</td>
+                <td>E</td>
+                <td>2.0</td>
+            </tr>
+            <tr>
+                <td>Less than 40%</td>
+                <td>F</td>
+                <td>0.0</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>I</td>
+                <td>Incomplete</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>W</td>
+                <td>Withdraw</td>
+            </tr>
+        </table>
+    </div>
+    <!--  -->
+    <!--  -->
+
+    <h1>Calculation of GPA/ CGPA</h1>
+    <p>Grade Point Average (GPA) or Cumulative Grade Point Average (CGPA) is the average of the grade points obtained in all the courses passed/completed by a student. For example, if a student passes/completes four courses in a semester having credit hours of C1, C2, C3 & C4 and his/her grade points in these courses are G1, G2, G3 & G4 respectively then Suppose a student got grade point "4.0" in a 3 Credit hours course and 3.0 in 1.5 credit hours course then his/her GPA/CGPA will be as follows</p>
+
+    <h1>Earned Credit Hours</h1>
+    <p>The course in which a student has obtained "D" or higher grade will be counted as earned credit hours. Any course in which a student has obtained "F" grade will not be counted towards his/her earned credit. In that case he/she will have to repeat the course. "F" grade will not be counted for GPA calculation.</p>
+
+    <div class="table_wrapper">
+        <table border="1">
+            <tr>
+                <th>Attendance (%)</th>
+                <th>Marks</th>
+                <th>Attendance (%)</th>
+                <th>Marks</th>
+            </tr>
+            <tr>
+                <td>90–100</td>
+                <td>10</td>
+                <td>65–69</td>
+                <td>5</td>
+            </tr>
+            <tr>
+                <td>85–89</td>
+                <td>9</td>
+                <td>60–64</td>
+                <td>4</td>
+            </tr>
+            <tr>
+                <td>80–84</td>
+                <td>8</td>
+                <td>55–59</td>
+                <td>3</td>
+            </tr>
+            <tr>
+                <td>75–79</td>
+                <td>7</td>
+                <td>50–54</td>
+                <td>2</td>
+            </tr>
+            <tr>
+                <td>70–74</td>
+                <td>6</td>
+                <td>45–49</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>Below 45%</td>
+                <td>0</td>
+            </tr>
+        </table>
+    </div>
+
+
+
+    <details>
+        <summary>Administration and Monitoring</summary>
+        <ul>
+            <li>
+                At the beginning of a semester, all the courses will be assigned by the Academic Committee comprising:
+                <ul>
+                    <li>Program Adviser</li>
+                    <li>Program Coordinator</li>
+                    <li>Two Senior Teachers</li>
+                </ul>
+            </li>
+
+
+            <li>The course-teacher shall provide the students a course outline indicating the list of textbooks to follow, probable dates of in-course tests and home assignments.</li>
+
+            <li>The Academic Committee will monitor the progress of the course by sitting together at least once in a month, fix up the examination schedule and conduct the examination.</li>
+
+            <li>Result of each course must be submitted by curse teacher within 10 working days of end of the exam.</li>
+
+            <li>Controller of exam will publish the result within one week after submission the result by the course teacher.</li>
+        </ul>
+    </details>
+
+
+    <details>
+        <summary>Academic Probation</summary>
+        <p>Students of undergraduate, graduate, postgraduate or any other academic and professional programs with a GPA less than 2.50 will be placed under probation for one semester only. If he/she fails to improve his/her grade within one semester, he/she will have to leave the university.</p>
+    </details>
+
+    <?php
+    require '../footer.php';
+    ?>
+</body>
+
+</html>
