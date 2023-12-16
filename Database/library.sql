f49b021c999c3290ccb52fdc7c91bb721a08fcc3
siMobin <shakibulislammobin@gmail.com>
2023-12-16 17:43:11 +0600

    Upload database for 8af4d9b
    	new file:   Database/library.sql

---
+++ Database/library.sql
@@ -0,0 +1,18 @@
+use StamfordUniversityBangladesh
+
+CREATE TABLE library_shelf (
+    serial INT PRIMARY KEY NOT NULL IDENTITY(1,1),
+    timeStamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    ISBN VARCHAR(255) NOT NULL,
+    Title VARCHAR(255) NOT NULL,
+    Author VARCHAR(255) NOT NULL,
+    publishYear INT,
+    edition VARCHAR(255),
+    Publisher VARCHAR(255),
+    shelfNumber VARCHAR(255),
+    department VARCHAR(255),
+    borrowerID VARCHAR(255),
+    borrowDate DATE,
+    returnDate DATE,
+    CONSTRAINT CK_Dates CHECK (borrowDate <= returnDate) -- Assuming borrowDate should be less than or equal to returnDate
+)
