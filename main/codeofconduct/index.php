f92182d534e2f954dc35df49432d126995910103
siMobin <shakibulislammobin@gmail.com>
2023-12-18 15:14:19 +0600

    Upload `Code of Conduct`
    	new file:   main/codeofconduct/index.php
    	new file:   main/style/codeofconduct.scss

---
+++ main/codeofconduct/index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="../images/logo.png">
+    <link rel="stylesheet" href="../style/codeofconduct.css">
+    <title>SUB-Code of Conduct</title>
+</head>
+
+<body>
+    <header>
+        <?php
+        require '../nav.php';
+        ?>
+    </header>
+
+    <details open>
+        <summary>Academic Rules & Regulations</summary>
+        <p>Stamford University Bangladesh is a Non-smoking and Non-political academic institution. Stamford University attaches top priority to maintain a healthy academic environment in the campus. Students of Stamford are expected to cooperate in achieving this goal to enhance their academic achievements through maintaining discipline, keeping the campus neat and clean and being good neighbors and model of good citizenship.</p>
+
+        <p>The academic rules and regulations regarding studying at Stamford University provide detailed information concerning class attendance, examination guidelines, student's dress, disciplinary matters etc. Students are advised to abide by the rules and regulations strictly. Failure to comply with university rules and regulations does not exempt students from whatever penalties/sanctions they may deserve.</p>
+    </details>
+
+
+    <details>
+        <summary>Class Attendance</summary>
+        <p>A student is expected to attend all the classes in each course. Maximum 20% absence in a course in one trimester may be exempted under emergency situation. It is the responsibility of the student to keep the course teacher informed regarding absence from classes. It would be the responsibility of the course teacher to announce the attendance policy at the beginning of the trimester. The course teacher will determine the manner by which, assignments or examinations missed by students, may be made up.</p>
+    </details>
+
+
+    <details>
+        <summary>Dress Code for the Students</summary>
+        <p>Every student of Stamford is required to dress himself/herself decently and properly for being a proud student of Stamford University. Students are advised not to enter the university campus or classroom in the following attire:</p>
+
+        <ul>
+            <li>Without properly pinned ID card.</li>
+            <li>Wearing a sleeveless, hanging t-shirt or chemise.</li>
+            <li>Wearing a wrack, ragged or slashed jeans/trousers.</li>
+            <li>Wearing unclean clothes.</li>
+            <li>Wearing half-pant or skirt.</li>
+            <li>Putting on slippers of ordinary type.</li>
+            <li>Wearing provocative and seductive clothing/dress.</li>
+        </ul>
+    </details>
+
+
+    <details>
+        <summary>Library Code</summary>
+        <strong>Students are advised to strictly observe the following code of conduct while using the library facility of the university:</strong>
+
+        <ul>
+            <li>Students must bear the library card while in the library.</li>
+            <li>Folder, files and bags etc. are not allowed in the library.</li>
+            <li>Students are permitted to take 02(two) different books at a time for ten days only. On expiry due date Tk.10/ only will be charged per day for each book.</li>
+            <li>Library remains open from <strong>9 a.m. to 8 p.m.</strong> except on holidays.</li>
+            <li>Students must return books on due date even if there is no class held on that day.</li>
+            <li>Library users must observe silence. All sorts of non academic discussions and unnecessary noise are strictly prohibited.</li>
+            <li>Smoking, eating and drinking (except fresh water) are strictly prohibited in the library.</li>
+            <li>In case of loss of the Library card a student has to deposit Tk. 50.00 only (for each) for a new one.</li>
+            <li>Library members may have their library card reissued against deposit (the damaged/old library card) for once only without any charge.</li>
+            <li>In case of loss of a book a student must replace the book by a new one or pay three times of the price of the book along with delay fine.</li>
+        </ul>
+
+        <p>For any kind of misconduct /violation of library rules and regulations there is a provision for holding up the library card/expulsion from membership. Students are to keep their personal belongings against a token. They are also requested not to keep any valuable materials (Cell phone, money, ID card etc.) in their bags. Library authority will not be responsible for missing any such items. If any student loses his/her token he/she must pay Tk. 100 only as fine. Students shall be held responsible for making any damage, page cutting or marking in the book; in such case he/she will have to replace the new copy. If a library book is found with a student outside the library without being issued, the student will be treated as a miscreant. In such a malpractice, the following penalty will be imposed on the concerned student:</p>
+
+        <ul>
+            <li>Library membership for one/more trimester will be held up.</li>
+            <li>Tk, 5000 to be realized as fine.</li>
+            <li>Examination for one trimester will be held up.</li>
+            <li>Result will remain withheld until fine amount is realized.</li>
+        </ul>
+    </details>
+
+
+
+    <details>
+        <summary class="major">Major Offenses</summary>
+        <strong>The following activities would be treated as committing major offenses:</strong>
+
+        <ul>
+            <li>Any activity that impedes, hinders, prevents the right of a student to attend classes.</li>
+            <li>Any act that disturbs the peace and order either in class room or in the campus.</li>
+            <li>Carrying any sort of deadly or dangerous weapon or explosive of any kind inside the university.</li>
+            <li>Possessing or taking any sort of drug or seductive product.</li>
+            <li>Bringing intoxicating drinks and /or regulated drugs inside the campus.</li>
+            <li>Willful disregard of authority, disrespect, discourtesy and disobedience to any of the faculty, officials and stuff of the university.</li>
+            <li>Any sort of physical assault and/or injuries to any one of the university.</li>
+            <li>Possession or distribution of any sort of pornographic materials.</li>
+            <li>Stealing any material of the university.</li>
+            <li>Extortion or unauthorized solicitation</li>
+            <li>Selling tickets, raffles or lotteries without permission of the university authority.</li>
+            <li>Using disrespectful or indecent language and committing offensive acts.</li>
+            <li>Any activity leading to defamation and maligning.</li>
+            <li>Destruction of university property or properties belonging to any member of the faculty and non-teaching staff</li>
+            <li>Any sort of forgery or falsification and attempt thereof.</li>
+            <li>Organizing gambling or playing cards inside the class room or campus.</li>
+            <li>Any activity that leads to misrepresentation and dishonesty.</li>
+            <li>Entering the class room and/or offices without proper permission.</li>
+            <li>Planning and organizing activities contrary to the best interest of the university.</li>
+            <li>Violating any clauses of the university circulation/notice.</li>
+            <li>Printing of any document/material using the names and seals of the university.</li>
+        </ul>
+    </details>
+
+
+    <details>
+        <summary class="minor">Minor Offenses</summary>
+        <strong>The following activities would be treated as committing minor offenses:</strong>
+        <ul>
+            <li>Entering the campus or class room without wearing ID card.</li>
+            <li>Smoking inside the class room or in the campus.</li>
+            <li>Manners unbecoming of a student of the university.</li>
+            <li>Violation of admission and registration procedures and policies.</li>
+            <li>Misuse or improper use of university properties and facilities.</li>
+            <li>Tampering, altering or destroying any part of the notices or posters.</li>
+            <li>Acts of any kind that causes panic and confusion in class room or in the campus.</li>
+            <li>Spitting on the floor, corridors, stairways which may result to dirty condition.</li>
+            <li>Loitering along the corridors, sitting on stairways, gates, exits and passages.</li>
+            <li>Committing any offense punishable under existing law of the land.</li>
+        </ul>
+    </details>
+
+
+    <details open>
+        <summary class="EDA">Execution of Disciplinary Action</summary>
+        <p>The Disciplinary Committee shall determine whether an offense committed by any student constitutes a major or a minor violation. It shall, likewise, determine the appropriate sanction/penalty against students who commit such a violation. Any of the following disciplinary actions may be imposed on students considering the nature and gravity of violation of the rules and regulations:</p>
+
+        <ul>
+            <li>Cancellation of a trimester.</li>
+            <li>Temporary suspension from the university.</li>
+            <li>Expulsion from the university.</li>
+        </ul>
+    </details>
+
+
+    <div class="notice"><strong>Note: </strong>
+        <p>
+            The university reserves the right to modify or change the academic rules and regulations as and when necessary. Such rules and regulations would go into force as determined by the proper authorities.</p>
+    </div>
+
+    <?php
+    require '../footer.php';
+    ?>
+</body>
+
+</html>
